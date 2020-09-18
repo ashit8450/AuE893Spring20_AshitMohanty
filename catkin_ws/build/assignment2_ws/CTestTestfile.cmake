7ba6565b6b8c554899cca3d04a8bcc6fb4995645
ashit8450 <amohant@clemson.edu>
2020-09-17 22:38:50 -0400

    Deleting Unecessary files

--- catkin_ws/build/assignment2_ws/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ashit/AuE893Spring20_AshitMohanty/catkin_ws/src/assignment2_ws
-# Build directory: /home/ashit/AuE893Spring20_AshitMohanty/catkin_ws/build/assignment2_ws
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
